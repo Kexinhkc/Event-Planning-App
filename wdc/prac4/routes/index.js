4b8b518331f8753fd863e3b6cecbd4b3cb3eebe1
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-04-02 07:31:30 +0000

    Sat, Apr 2, 2022, 7:31 AM +00:00

--- wdc/prac4/routes/index.js
+++ wdc/prac4/routes/index.js
@@ -12,7 +12,7 @@ router.get('/last.txt', function(req, res, next) {
 
     if (count==0){
       count++;
-      res.send(time);
+      res.send(time)
     }else{
       count++;
       time = new Date();
