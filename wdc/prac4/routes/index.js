026c5ee370e6b5699f221dbfe20a03ae056821d7
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-04-02 05:21:51 +0000

    Sat, Apr 2, 2022, 5:21 AM +00:00

--- wdc/prac4/routes/index.js
+++ wdc/prac4/routes/index.js
@@ -6,21 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-var count = 0;
-var time = '';
-router.get('/last.txt', function(req, res, next) {
-
-  if(count == 0){
-    res.send(' ');
-    time = Date.now();
-    count++;
-    res.send({time});
-  }else{
-    count++;
-    res.send(time);
-    time = Date.now();
-  }
-
-});
-
 module.exports = router;
