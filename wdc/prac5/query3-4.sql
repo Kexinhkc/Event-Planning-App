8ce8db45f62829be99f864ee003ef610fa19965e
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-04-25 07:37:27 +0000

    Mon, Apr 25, 2022, 7:37 AM +00:00

--- wdc/prac5/query3-4.sql
+++ wdc/prac5/query3-4.sql
@@ -1,4 +1,4 @@
-SELECT given_name,family_name, Students.student_id FROM Students
+SELECT given_name,family_name FROM Students
 INNER JOIN Enrolments
 ON Students.student_id=Enrolments.student_id
 WHERE subject_code = 'COMP SCI 2207';
