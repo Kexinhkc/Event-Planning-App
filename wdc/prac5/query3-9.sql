56b7ea4e6c416d86cf41a4f2c344886587b304bc
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-04-25 14:20:54 +0000

    Mon, Apr 25, 2022, 2:20 PM +00:00

--- wdc/prac5/query3-9.sql
+++ wdc/prac5/query3-9.sql
@@ -3,7 +3,7 @@ INNER JOIN Subjects
 ON Enrolments.subject_code = Subjects.subject_code
 INNER JOIN Teachers
 ON Subjects.teacher_id=Teachers.staff_id
-WHERE student_id='a1111113';
+WHERE student_id='a1111113' AND Teachers.faculty ='ECMS';
 
 
 
