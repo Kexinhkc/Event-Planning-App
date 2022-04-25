93f833b1a1052966fdb695532b2c629d9b49ea87
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-04-25 15:09:44 +0000

    Mon, Apr 25, 2022, 3:09 PM +00:00

--- wdc/prac5/query3-10.sql
+++ wdc/prac5/query3-10.sql
@@ -1,5 +1,3 @@
-for each student taking one or more subjects outside of the ECMS faculty, lists the student's name (given, family), the student's program/degree, the name of the subject, and which faculty the subject is run by.
-
 SELECT Students.given_name,Students.family_name,Students.program,Subjects.subject,Subjects.faculty
 FROM Enrolments
 INNER JOIN Subjects
@@ -9,6 +7,3 @@ ON Students.student_id=Enrolments.student_id
 WHERE Subjects.Subject_code ='PHIL 2039' OR Subjects.Subject_code ='ENTREP 1011' OR Subjects.Subject_code ='BIOCHEM 2500'
 ;
 
-INNER JOIN Subjects
-ON Enrolments.subject_code=Subjects.subject_code
-Students.given_name,Students.family_name,Students.program,Subjects.subject,Subjects.faculty
