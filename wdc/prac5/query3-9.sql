2839b5fd014e4b565f540cb7d8bc86cdf64f1444
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-04-25 13:58:11 +0000

    Mon, Apr 25, 2022, 1:58 PM +00:00

--- wdc/prac5/query3-9.sql
+++ wdc/prac5/query3-9.sql
@@ -1,1 +1,6 @@
-find out subjects code he studied 
+find out subjects code he studied
+join the table with subjects, get teacher_id
+join the table with teachers table, get name
+
+SELECT subject_code FROM Enrolments
+WHERE student_id ='a1111113';
