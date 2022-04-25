936f30dc53d4f2bce9e96b4021d17585d3883d00
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-04-25 13:18:27 +0000

    Mon, Apr 25, 2022, 1:18 PM +00:00

--- wdc/prac5/query3-5.sql
+++ wdc/prac5/query3-5.sql
@@ -1,4 +1,4 @@
 SELECT given_name, family_name, Enrolments.mark FROM Students
 INNER JOIN Enrolments
 ON Students.student_id=Enrolments.student_id
-WHERE subject_code = 'COMP SCI 2000' ;
+WHERE subject_code = 'COMP SCI 2000' AND mark < 50;
