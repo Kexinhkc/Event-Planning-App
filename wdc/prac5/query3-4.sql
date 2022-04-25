c9aed658ffe994f666b759a873aa0626c53cd18f
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-04-25 07:14:16 +0000

    Mon, Apr 25, 2022, 7:14 AM +00:00

--- wdc/prac5/query3-4.sql
+++ wdc/prac5/query3-4.sql
@@ -1,4 +1,4 @@
-SELECT given_name,family_name FROM Students
+SELECT given_name,family_name, Students.student_id FROM Students
 INNER JOIN Enrolments
 ON Students.student_id=Enrolments.student_id
-WHERE program = 'BCompSc';
+WHERE Subjects = 'Web and Database Computing';
