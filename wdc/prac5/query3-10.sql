f6315455f04611e4543acb409903ccdd81db43cb
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-04-25 14:31:17 +0000

    Mon, Apr 25, 2022, 2:31 PM +00:00

---
+++ wdc/prac5/query3-10.sql
@@ -0,0 +1,3 @@
+for each student taking one or more subjects outside of the ECMS faculty, lists the student's name (given, family), the student's program/degree, the name of the subject, and which faculty the subject is run by.
+
+SELECT given_name,family_name,program,subject,faculty
