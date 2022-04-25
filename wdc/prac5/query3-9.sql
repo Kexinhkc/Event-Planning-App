0d1a56ab4506e651c323307d0f13af10358991cf
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-04-25 14:02:12 +0000

    Mon, Apr 25, 2022, 2:02 PM +00:00

--- wdc/prac5/query3-9.sql
+++ wdc/prac5/query3-9.sql
@@ -2,7 +2,7 @@ find out subjects code he studied
 join the table with subjects, get teacher_id
 join the table with teachers table, get name
 
-SELECT subject_code FROM Enrolments
+SELECT Teachers.staff_id, Teachers.given_name, Teachers.family_name FROM Enrolments
 INNER JOIN Subjects
 ON Enrolments.student_id = Subjects.student_id
 WHERE student_id='a1111113';
