86a9c6e2853bc9e91ad1561b124d32ef389b6f6d
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-04-25 14:06:04 +0000

    Mon, Apr 25, 2022, 2:06 PM +00:00

--- wdc/prac5/query3-9.sql
+++ wdc/prac5/query3-9.sql
@@ -5,4 +5,6 @@ join the table with teachers table, get name
 SELECT Teachers.staff_id, Teachers.given_name, Teachers.family_name FROM Enrolments
 INNER JOIN Subjects
 ON Enrolments.subject_code = Subjects.subject_code
-WHERE student_id='a1111113';
+WHERE student_id='a1111113'
+INNER JOIN Teachers
+ON ;
