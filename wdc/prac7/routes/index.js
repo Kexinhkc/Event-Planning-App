294c59fc78783b7392d1e7f580f7a3bdc8b6c114
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-20 11:13:44 +0000

    Fri, May 20, 2022, 11:13 AM +00:00

--- wdc/prac7/routes/index.js
+++ wdc/prac7/routes/index.js
@@ -50,14 +50,6 @@ router.post('/combine', function(req, res, next){
 
   });
 
-/* 2.2 */
-router.post('/*', function(req, res, next){
-  if (req.method==="POST"){
-console.log("POST from a user");
-  }
-next();
-
-});
 
 /* 3.1 */
 router.get('/cookie',function(req,res,next){
--- wdc/prac7/routes/users.js
+++ wdc/prac7/routes/users.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* 2.2 */
+router.post('/*', function(req, res, next){
+  if (req.method==="POST"){
+console.log("POST from a user");
+  }
+next();
+
+});
+
 module.exports = router;
