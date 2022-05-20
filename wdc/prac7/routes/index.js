71b6f74efe3345954c45b7fec341e6d24add6c58
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-20 08:52:53 +0000

    Fri, May 20, 2022, 8:52 AM +00:00

--- wdc/prac7/routes/index.js
+++ wdc/prac7/routes/index.js
@@ -10,7 +10,12 @@ router.get('/brew', function(req, res, next) {
   var drink = req.query.drink;
 
   if (drink == "tea"){
-    
+    res.send('A delicious cup of tea!');
+  }else if (drink == "coffee"){
+    res.sendStatus(418);
+  }else{
+    res.sendStatus(400);
+  }
   }
 });
 
