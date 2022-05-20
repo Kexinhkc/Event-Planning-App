bbb1a2a1b09fe666ad51b3cbd9760f6b7981af41
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-20 11:17:50 +0000

    Fri, May 20, 2022, 11:17 AM +00:00

--- wdc/prac7/routes/users.js
+++ wdc/prac7/routes/users.js
@@ -6,6 +6,22 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* 1.4 */
+var posts=[];
+
+router.post('/addpost', function(req, res, next){
+    if ("title" in req.body && "content" in req.body) {
+        var post;
+        post=req.body;
+        posts.unshift(post);
+        res.end();
+    }
+    else {
+        res.sendStatus(400);
+    }
+
+});
+
 /* 2.2 */
 router.post('/*', function(req, res, next){
   if (req.method==="POST"){
