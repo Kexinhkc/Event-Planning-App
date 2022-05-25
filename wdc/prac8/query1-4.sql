b227e3f1d8b0084539a36ecbce73c88e19b71474
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-25 15:02:32 +0000

    Wed, May 25, 2022, 3:02 PM +00:00

--- wdc/prac8/query1-4.sql
+++ wdc/prac8/query1-4.sql
@@ -5,14 +5,14 @@ select film_id
 FROM film
 WHERE title = 'TWISTED PIRATES';
 
-select *
+select address
 From inventory AS i
 INNER JOIN store AS s
 ON i.store_id = s.store_id
 INNER JOIN address AS a
 ON s.address_id=a.address_id
 WHERE film_id = 918
-
+LIMIT 1;
 ;
 
 
