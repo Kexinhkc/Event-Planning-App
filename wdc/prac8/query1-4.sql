b57ca4a832ab5172d17ae9d9d92453d97d593827
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-25 15:04:33 +0000

    Wed, May 25, 2022, 3:04 PM +00:00

--- wdc/prac8/query1-4.sql
+++ wdc/prac8/query1-4.sql
@@ -1,9 +1,3 @@
-street number/name
-street addresses(s) of stores that have a copy of the film TWISTED PIRATES in their inventory
-
-select film_id
-FROM film
-WHERE title = 'TWISTED PIRATES';
 
 select address
 From inventory AS i
@@ -14,8 +8,3 @@ ON s.address_id=a.address_id
 WHERE film_id = 918
 LIMIT 1;
 ;
-
-
-select address
-FROM address
-LIMIT 50;
