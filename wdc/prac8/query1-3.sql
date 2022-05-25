0ea0aab1f75df7dccac0c41093f845bb4a652db4
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-25 05:53:50 +0000

    Wed, May 25, 2022, 5:53 AM +00:00

--- wdc/prac8/query1-3.sql
+++ wdc/prac8/query1-3.sql
@@ -1,4 +1,6 @@
-SELECT first_name,last_name
-FROM actor
+SELECT first_name,last_name,rental_date
+FROM rental
+INNER JOIN customer
+ON rental.customer_id = customer.customer_id;
 
 //first name,last name, rental_date
