aaaa02cadb171366ecacbc744af9a0ced63309c3
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-25 06:58:47 +0000

    Wed, May 25, 2022, 6:58 AM +00:00

--- wdc/prac8/query1-3.sql
+++ wdc/prac8/query1-3.sql
@@ -1,8 +1,8 @@
-SELECT first_name,last_name,rental_date
+SELECT first_name,last_name,MAX(rental_date)
 FROM rental
 INNER JOIN customer
 ON rental.customer_id = customer.customer_id
-GROUP BY rental.customer_id
+GROUP BY rental.customer_id;
 
 
  name of the person with the longest standing current rental, not yet return
