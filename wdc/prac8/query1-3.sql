d654d4155f391e254a44bbd8c53ad4ef55e7c125
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-25 08:38:47 +0000

    Wed, May 25, 2022, 8:38 AM +00:00

--- wdc/prac8/query1-3.sql
+++ wdc/prac8/query1-3.sql
@@ -1,4 +1,4 @@
-SELECT DATEDIFF(millisecond,rental.return_date,rental.rental_date) AS DateDiff
+SELECT TIMESTAMPDIFF(MINUTE,return_date,rental_date) AS DateDiff
 FROM rental;
 
 DATEDIFF(r.return_date,r.rental_date)
