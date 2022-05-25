2d810863dc131e003185464e7d26fccaaf77a730
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-25 07:20:37 +0000

    Wed, May 25, 2022, 7:20 AM +00:00

--- wdc/prac8/query1-3.sql
+++ wdc/prac8/query1-3.sql
@@ -4,7 +4,8 @@ INNER JOIN customer
 ON rental.customer_id = customer.customer_id
 GROUP BY rental.customer_id
 WHERE MIN(rental_date);
-ORDER BY salary 
+ORDER BY rental_date DESC
+WHERE ROWNUM = 1;
 
 
 
