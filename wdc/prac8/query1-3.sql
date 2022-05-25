d20b8f8e617b95f4feec6a6032fdd0dafe153b17
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-25 13:55:30 +0000

    Wed, May 25, 2022, 1:55 PM +00:00

--- wdc/prac8/query1-3.sql
+++ wdc/prac8/query1-3.sql
@@ -6,6 +6,7 @@ DATEDIFF(r.return_date,r.rental_date)
 
 SELECT rental_date,return_date
 FROM rental
+WHERE return_date = NULL
 LIMIT 100,100;
 
  name of the person with the longest standing current rental, not yet return
