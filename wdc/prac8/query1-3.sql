a5b70dfa7fe90c33cd435dd6c594b89a4ec0d4d6
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-25 06:00:19 +0000

    Wed, May 25, 2022, 6:00 AM +00:00

--- wdc/prac8/query1-3.sql
+++ wdc/prac8/query1-3.sql
@@ -2,5 +2,5 @@ SELECT first_name,last_name,rental_date
 FROM rental
 INNER JOIN customer
 ON rental.customer_id = customer.customer_id
-WHERE MAX(rental_date);
+WHERE rental.rental_date = MAX(rental_date);
 
