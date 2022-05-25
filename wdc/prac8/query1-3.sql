4c0aba597bdd2225e18ff5464d3153916b7b58c0
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-25 14:11:02 +0000

    Wed, May 25, 2022, 2:11 PM +00:00

--- wdc/prac8/query1-3.sql
+++ wdc/prac8/query1-3.sql
@@ -1,10 +1,7 @@
 SELECT customer_id,MAX(TIMESTAMPDIFF(MINUTE,rental_date,return_date))
 FROM rental;
 
-
-DATEDIFF(r.return_date,r.rental_date)
-
-SELECT rental_date,return_date
+SELECT first_name,last_name, rental_date
 FROM rental
 WHERE return_date = null
 ;
