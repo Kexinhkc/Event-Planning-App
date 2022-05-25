7cc3eacfb6af5a72656e968129e13e8d26865d97
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-25 06:51:33 +0000

    Wed, May 25, 2022, 6:51 AM +00:00

--- wdc/prac8/query1-3.sql
+++ wdc/prac8/query1-3.sql
@@ -4,3 +4,5 @@ INNER JOIN customer
 ON rental.customer_id = customer.customer_id
 WHERE rental.rental_date = MAX(rental_date);
 
+
+ name of the person with the longest standing current rental, not yet return
