7a89afe3f791d85d28d570c5ad6659dc8ad4f305
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-25 07:26:59 +0000

    Wed, May 25, 2022, 7:26 AM +00:00

--- wdc/prac8/query1-3.sql
+++ wdc/prac8/query1-3.sql
@@ -2,8 +2,6 @@ SELECT first_name,last_name,MIN(rental_date)
 FROM rental
 INNER JOIN customer
 ON rental.customer_id = customer.customer_id;
-GROUP BY rental.customer_id
-WHERE rental_date = (SELECT MIN(rental_date) FROM  rental);
 
 
 
