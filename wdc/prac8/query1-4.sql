7c4b8ee0f765f083b119ccd181f6ae6d52b073fa
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-25 15:00:57 +0000

    Wed, May 25, 2022, 3:00 PM +00:00

--- wdc/prac8/query1-4.sql
+++ wdc/prac8/query1-4.sql
@@ -9,9 +9,11 @@ select *
 From inventory AS i
 INNER JOIN store AS s
 ON i.store_id = s.store_id
-WHERE film_id = 918
 INNER JOIN address AS a
-ON s.address_id=a.address_id;
+ON s.address_id=a.address_id
+WHERE film_id = 918
+
+;
 
 
 select address
