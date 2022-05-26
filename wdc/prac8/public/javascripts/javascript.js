bff486285c43d7e82cf1075411feaac14f7b1355
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-26 03:15:18 +0000

    Thu, May 26, 2022, 3:15 AM +00:00

--- wdc/prac8/public/javascripts/javascript.js
+++ wdc/prac8/public/javascripts/javascript.js
@@ -2,6 +2,7 @@ function loadPage() {
     var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+        JSON.parse(this.responseText)
         document.getE
     }
   };
