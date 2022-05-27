8a4c3655fac4460e205dadb936b42c9c50857fa9
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-27 03:49:16 +0000

    Fri, May 27, 2022, 3:49 AM +00:00

--- wdc/prac8/public/javascripts/javascript.js
+++ wdc/prac8/public/javascripts/javascript.js
@@ -3,22 +3,22 @@ function loadPage() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        var actors = JSON.parse(this.responseText);
-        var table = document.getElementsByTagName("tbody");
+        // var actors = JSON.parse(this.responseText);
+        // var table = document.getElementsByTagName("tbody");
 
         console.log("okay");
 
-        for (let item in actors){
-            console.log(actors.first_name);
-            console.log(actors.last_name);
+        // for (let item in actors){
+        //     console.log(actors.first_name);
+        //     console.log(actors.last_name);
 
-            var row = document.createElement("tr");
-            var cell1 = document.createElement("td");
-            cell1.innerText = item.first_name;
-            var cell2 = document.createElement("td");
-            cell2.innerText = item.last_name;
-            document.row.appendChild(cell2);
-            document.table.appendChild(row);
+        //     var row = document.createElement("tr");
+        //     var cell1 = document.createElement("td");
+        //     cell1.innerText = item.first_name;
+        //     var cell2 = document.createElement("td");
+        //     cell2.innerText = item.last_name;
+        //     document.row.appendChild(cell2);
+        //     document.table.appendChild(row);
         }
     };
     xhttp.open("GET", "/actors.html", true);
