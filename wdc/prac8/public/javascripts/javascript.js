c2dd3e66f29b756b2f084a50f94c1e6b2712a6c2
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-27 06:12:21 +0000

    Fri, May 27, 2022, 6:12 AM +00:00

--- wdc/prac8/public/javascripts/javascript.js
+++ wdc/prac8/public/javascripts/javascript.js
@@ -33,8 +33,8 @@ function loadPage() {
 function addActor() {
 
   let actor = {
-    firstname: document.getElementByID('actor-first-name')[0].value,
-    lastname: document.getElementByID('actor-last-name')[0].value
+    firstname: document.getElementById('actor-first-name')[0].value,
+    lastname: document.getElementById('actor-last-name')[0].value
   };
 
   let xhttp = new XMLHttpRequest();
