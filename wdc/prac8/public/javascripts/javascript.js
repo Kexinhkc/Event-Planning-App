3a25ccd8388b5daa85a2d5f6e8326bdd3e753d5c
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-27 14:17:35 +0000

    Fri, May 27, 2022, 2:17 PM +00:00

--- wdc/prac8/public/javascripts/javascript.js
+++ wdc/prac8/public/javascripts/javascript.js
@@ -47,11 +47,11 @@ function addActor() {
 
   var xhttp = new XMLHttpRequest();
 
-  // xhttp.onreadystatechange = function () {
-  //     if (this.readyState == 4 && this.status == 200) {
-  //         alert("Actor added successfully!");
-  //     }
-  // }
+  xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+          alert("Actor added successfully!");
+      }
+  }
 
   xhttp.open("POST", "/add", true);
   xhttp.setRequestHeader("Content-type", "application/json");
