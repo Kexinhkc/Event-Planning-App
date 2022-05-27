7e934bd5dd0bd1dc7648676b33bece5f7dac0185
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-27 05:14:12 +0000

    Fri, May 27, 2022, 5:14 AM +00:00

--- wdc/prac8/public/javascripts/javascript.js
+++ wdc/prac8/public/javascripts/javascript.js
@@ -7,20 +7,20 @@ function loadPage() {
          var actors =JSON.parse(this.responseText);
          var table = document.getElementsByTagName("tbody");
 
-        // console.log(actors[0].last_name)
+        console.log(actors[0].last_name)
 
         for (let item in actors){
-            console.log(actors.first_name);
-            console.log(actors.last_name);
+            // console.log(item.first_name);
+            // console.log(item.last_name);
 
             var row = document.createElement("tr");
             var cell1 = document.createElement("td");
             cell1.innerText = item.first_name;
-            document.row.appendChild(cell1);
+            row.appendChild(cell1);
             var cell2 = document.createElement("td");
             cell2.innerText = item.last_name;
-            document.row.appendChild(cell2);
-            document.table.appendChild(row);
+            row.appendChild(cell2);
+            table[0].appendChild(row);
         }
     };
     xhttp.open("GET", "/getActors", true);
