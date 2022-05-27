2731182552b852af18807c322e551abc2e450852
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-27 12:12:46 +0000

    Fri, May 27, 2022, 12:12 PM +00:00

--- wdc/prac8/routes/index.js
+++ wdc/prac8/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('actors', { title: 'Actors' });
+  res.render('index', { title: 'Express' });
 });
 
 
