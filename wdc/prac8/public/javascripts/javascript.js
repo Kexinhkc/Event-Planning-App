e22da926b12330032d80ff441dcf6169a5208fff
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-05-27 04:09:40 +0000

    Fri, May 27, 2022, 4:09 AM +00:00

--- wdc/prac8/public/javascripts/javascript.js
+++ wdc/prac8/public/javascripts/javascript.js
@@ -24,5 +24,16 @@ function loadPage() {
     xhttp.open("GET", "/actors.html", true);
     xhttp.send();
   }
-}
+
+  function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("demo").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "demo_get.asp", true);
+    xhttp.send();
+  }
+
 
