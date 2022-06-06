38ab8846cedd62e3f8b9c70ff44ba191ff9b97c6
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-06 20:16:39 +0000

    Mon, Jun 6, 2022, 8:16 PM +00:00

---
+++ wdc/WDCdebugproject/public/eventPlanning.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE myDatabase;
+
+USE myDatabase;
+
+CREATE TABLE admin (
+    email VARCHAR(127),
+    password VARCHAR(127),
+    PRIMARY KEY (email)
+);
+
+CREATE TABLE users (
+    users_email VARCHAR(127) UNIQUE,
+    password VARCHAR(127),
+    last_name VARCHAR(63),
+    first_name VARCHAR(63),
+    PRIMARY KEY (users_email)
+);
+
+CREATE TABLE events (
+    ID INT NOT NULL AUTO_INCREMENT,
+    event_name VARCHAR(127),
+    description VARCHAR(1207),
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE creating_trip (
+    users_email VARCHAR(127),
+    event_ID INT,
+    FOREIGN KEY (users_email) REFERENCES users(users_email) ON DELETE SET NULL,
+    FOREIGN KEY (event_ID) REFERENCES events(ID) ON DELETE SET NULL
+);
+
+CREATE TABLE availaility (
+    timestamp DATETIME,
+    event_ID INT,
+    FOREIGN KEY (event_ID) REFERENCES events(ID) ON DELETE CASCADE
+);
+
