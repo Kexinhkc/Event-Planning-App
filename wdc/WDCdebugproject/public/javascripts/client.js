dfec8690523d78953d0028a57356f6c20c6d0299
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-06 18:50:50 +0000

    Mon, Jun 6, 2022, 6:50 PM +00:00

--- wdc/WDCdebugproject/public/javascripts/client.js
+++ wdc/WDCdebugproject/public/javascripts/client.js
@@ -1,6 +1,15 @@
 function onSignIn(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token;
-    console.log(id_token)
+    // console.log(id_token)
+    if (auth2.isSignedIn.get()) {
+      var profile = auth2.currentUser.get().getBasicProfile();
+      console.log('ID: ' + profile.getId());
+      console.log('Full Name: ' + profile.getName());
+      console.log('Given Name: ' + profile.getGivenName());
+      console.log('Family Name: ' + profile.getFamilyName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+    }
 
     // var xhr = new XMLHttpRequest();
     // xhr.open('POST', 'https://yourbackend.example.com/tokensignin');
