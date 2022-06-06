7d1c7712e59ff0536569f629e4a24221e02c867e
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-06 21:12:59 +0000

    Mon, Jun 6, 2022, 9:12 PM +00:00

--- wdc/WDCdebugproject/public/javascripts/client.js
+++ wdc/WDCdebugproject/public/javascripts/client.js
@@ -1,18 +1,15 @@
-function onSignIn(googleUser) {
-    var id_token = googleUser.getAuthResponse().id_token;
-    // console.log(id_token)
-    // if (auth2.isSignedIn.get()) {
-    //   var profile = auth2.currentUser.get().getBasicProfile();
-    //   console.log('ID: ' + profile.getId());
-    //   console.log('Full Name: ' + profile.getName());
-    //   console.log('Given Name: ' + profile.getGivenName());
-    //   console.log('Family Name: ' + profile.getFamilyName());
-    //   console.log('Image URL: ' + profile.getImageUrl());
-    //   console.log('Email: ' + profile.getEmail());
-    // }
+function onSignIn(response) {
+  const responsePayload = decodeJwtResponse(response.credential);
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.open('POST', '',true);
-    xhttp.setRequestHeader('Content-Type', 'application/json');
-    xhttp.send(JSON.stringify('id_token'));
+  console.log("ID: " + responsePayload.sub);
+  console.log('Full Name: ' + responsePayload.name);
+  console.log('Given Name: ' + responsePayload.given_name);
+  console.log('Family Name: ' + responsePayload.family_name);
+  console.log("Image URL: " + responsePayload.picture);
+  console.log("Email: " + responsePayload.email);
+
+    // var xhttp = new XMLHttpRequest();
+    // xhttp.open('POST', '',true);
+    // xhttp.setRequestHeader('Content-Type', 'application/json');
+    // xhttp.send(JSON.stringify('id_token'));
   }
