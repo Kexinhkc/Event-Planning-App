f05a6e21306d638e44161516a026f5edfcd45acd
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-07 09:55:08 +0000

    Tue, Jun 7, 2022, 9:55 AM +00:00

--- wdc/WDCdebugproject/public/javascripts/client.js
+++ wdc/WDCdebugproject/public/javascripts/client.js
@@ -8,21 +8,22 @@
 //  return JSON.parse(jsonPayload);
 // };
 
-// function onSignIn(response) {
+function onSignIn(response) {
 
-//      console.log(JSON.stringify(parseJwt(response.credential)));
+    // console.log(JSON.stringify(parseJwt(response.credential)));
 
-//   // const responsePayload = decodeJwtResponse(response.credential);
+  //const responsePayload = decodeJwtResponse(response.credential);
 
-//   // console.log("ID: " + responsePayload.sub);
-//   // console.log('Full Name: ' + responsePayload.name);
-//   // console.log('Given Name: ' + responsePayload.given_name);
-//   // console.log('Family Name: ' + responsePayload.family_name);
-//   // console.log("Image URL: " + responsePayload.picture);
-//   // console.log("Email: " + responsePayload.email);
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + responsePayload.sub);
+  console.log('Full Name: ' + responsePayload.name);
+  console.log('Given Name: ' + responsePayload.given_name);
+  console.log('Family Name: ' + responsePayload.family_name);
+  console.log("Image URL: " + responsePayload.picture);
+  console.log("Email: " + responsePayload.email);
 
 //     // var xhttp = new XMLHttpRequest();
 //     // xhttp.open('POST', '',true);
 //     // xhttp.setRequestHeader('Content-Type', 'application/json');
 //     // xhttp.send(JSON.stringify('id_token'));
-//   }
+  }
