5a1439d5d731641531eb2e7d0f6d6977c0cb6998
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-07 10:48:40 +0000

    Tue, Jun 7, 2022, 10:48 AM +00:00

--- wdc/WDCdebugproject/routes/index.js
+++ wdc/WDCdebugproject/routes/index.js
@@ -10,20 +10,25 @@ router.get('/', function(req, res, next) {
 
 router.post('/login', function(req, res, next) {
   if ('token' in req.body) {
-    
-    async function verify() {
-      const ticket = await client.verifyIdToken({
-          idToken: req.body.token,
-          audience: '1006345318538-06u5fqd3sur2bthaea307jtjalc33f3r.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
-          // Or, if multiple clients access the backend:
-          //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-      });
-      const payload = ticket.getPayload();
-      const userid = payload['sub'];
-      console.log(userid);
-    }
-    verify().catch(console.error);
+
+        async function verify() {
+          const ticket = await client.verifyIdToken({
+              idToken: req.body.token,
+              audience: '1006345318538-06u5fqd3sur2bthaea307jtjalc33f3r.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
+              // Or, if multiple clients access the backend:
+              //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+          });
+          const payload = ticket.getPayload();
+          const userid = payload['sub'];
+          console.log(userid)
+        }
+        verify().catch(console.error);
+
+  } else {
+    console.log('bad request');
+    res.sendStatus(400);
   }
+
 });
 
 
