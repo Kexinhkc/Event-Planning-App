14d64e58e31c0a0229d7eff8e1d9b8c12857f94f
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-07 03:56:06 +0000

    Tue, Jun 7, 2022, 3:56 AM +00:00

--- wdc/WDCdebugproject/public/javascripts/client.js
+++ wdc/WDCdebugproject/public/javascripts/client.js
@@ -1,28 +1,28 @@
-function parseJwt (token) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
- var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-   return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
- }).join(''));
+// function parseJwt (token) {
+//   var base64Url = token.split('.')[1];
+//   var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+//  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+//    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+//  }).join(''));
 
- return JSON.parse(jsonPayload);
-};
+//  return JSON.parse(jsonPayload);
+// };
 
-function onSignIn(response) {
+// function onSignIn(response) {
 
-     console.log(JSON.stringify(parseJwt(response.credential)));
+//      console.log(JSON.stringify(parseJwt(response.credential)));
 
-  // const responsePayload = decodeJwtResponse(response.credential);
+//   // const responsePayload = decodeJwtResponse(response.credential);
 
-  // console.log("ID: " + responsePayload.sub);
-  // console.log('Full Name: ' + responsePayload.name);
-  // console.log('Given Name: ' + responsePayload.given_name);
-  // console.log('Family Name: ' + responsePayload.family_name);
-  // console.log("Image URL: " + responsePayload.picture);
-  // console.log("Email: " + responsePayload.email);
+//   // console.log("ID: " + responsePayload.sub);
+//   // console.log('Full Name: ' + responsePayload.name);
+//   // console.log('Given Name: ' + responsePayload.given_name);
+//   // console.log('Family Name: ' + responsePayload.family_name);
+//   // console.log("Image URL: " + responsePayload.picture);
+//   // console.log("Email: " + responsePayload.email);
 
-    // var xhttp = new XMLHttpRequest();
-    // xhttp.open('POST', '',true);
-    // xhttp.setRequestHeader('Content-Type', 'application/json');
-    // xhttp.send(JSON.stringify('id_token'));
-  }
+//     // var xhttp = new XMLHttpRequest();
+//     // xhttp.open('POST', '',true);
+//     // xhttp.setRequestHeader('Content-Type', 'application/json');
+//     // xhttp.send(JSON.stringify('id_token'));
+//   }
--- wdc/WDCdebugproject/routes/index.js
+++ wdc/WDCdebugproject/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'landingPage' });
 });
 
+router.post('/login.html', function(req, res, next) {
+  var id = req.credentials;
+  console.log(id)
+});
+
 module.exports = router;
