b0640a580b9abf88c7c2ba24e06396ddb063a915
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-07 13:20:52 +0000

    Tue, Jun 7, 2022, 1:20 PM +00:00

--- wdc/WDCdebugproject/routes/index.js
+++ wdc/WDCdebugproject/routes/index.js
@@ -38,7 +38,7 @@ router.post('/login', function(req, res, next) {
               return;
             }
 
-            let query = "SELECT first_name,last_name,email FROM users WHERE email = ?;";
+            let query = "SELECT first_name,last_name,users_email FROM users WHERE users_email = ?;";
             connection.query(query,[email],function(error, rows, fields) {
               connection.release(); // release connection
               if (error) {
