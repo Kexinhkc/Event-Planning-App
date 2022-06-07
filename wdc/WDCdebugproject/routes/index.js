3f1ed9fa4db2bb554d9781e822aee98470bfda35
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-07 10:26:12 +0000

    Tue, Jun 7, 2022, 10:26 AM +00:00

--- wdc/WDCdebugproject/routes/index.js
+++ wdc/WDCdebugproject/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client("1006345318538-06u5fqd3sur2bthaea307jtjalc33f3r.apps.googleusercontent.com");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
