51facdb6c6d947099a308662962496952b15a57d
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-07 10:28:39 +0000

    Tue, Jun 7, 2022, 10:28 AM +00:00

--- wdc/WDCdebugproject/routes/index.js
+++ wdc/WDCdebugproject/routes/index.js
@@ -8,10 +8,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'landingPage' });
 });
 
-router.post('/home.html', function(req, res, next) {
-  var id = req.body.credentials;
-  console.log(id)
-  // res.end();
+router.post('/login', function(req, res, next) {
+  if ('token' in req.body) {
+    async function verify() {
+      const ticket = await client.verifyIdToken({
+          idToken: token,
+          audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+          // Or, if multiple clients access the backend:
+          //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+      });
+      const payload = ticket.getPayload();
+      const userid = payload['sub'];
+      // If request specified a G Suite domain:
+      // const domain = payload['hd'];
+    }
+    verify().catch(console.error);
+  }
 });
 
 
