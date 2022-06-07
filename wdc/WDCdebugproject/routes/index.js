1ba6a77e7b2d21952c276403a3fcabff57184ea6
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-07 04:05:30 +0000

    Tue, Jun 7, 2022, 4:05 AM +00:00

--- wdc/WDCdebugproject/routes/index.js
+++ wdc/WDCdebugproject/routes/index.js
@@ -6,10 +6,30 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'landingPage' });
 });
 
-router.post('/login.html', function(req, res, next) {
-  var id = req.body.credentials;
-  console.log(id)
-  res.end();
-});
+// router.post('/login.html', function(req, res, next) {
+//   var id = req.body.credentials;
+//   console.log(id)
+//   res.end();
+// });
+
+router.post('/login', (req, res) => {
+  const { password, token } = req.body.data;
+  jwt.verify(token, process.env.JWT_WEB_SECRET, (err, decoded) => {
+  if(err){
+  res.status(401).json({errors: "Invalid token"})
+  }
+  else{
+  res.json({});
+  User.findOne({ _id: decoded._id }).then(user => {
+  if(user) {
+  user.setPassword(password);
+  user.save().then(() => res.json({}));
+  } else {
+  res.status(400).json({ errors: { global : "Invalid token" }})
+  }
+  })
+  }
+  });
+  });
 
 module.exports = router;
