6bda7d0b43cfb60f5e82deff741667104a91e020
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-08 03:50:23 +0000

    Wed, Jun 8, 2022, 3:50 AM +00:00

--- wdc/WDCdebugproject/public/javascripts/client.js
+++ wdc/WDCdebugproject/public/javascripts/client.js
@@ -11,7 +11,8 @@ function onSignIn(googleUser) {
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             alert("Login Successfully");
-            window.location.replace("public/home.html");
+           // window.location.replace("public/home.html");
+            window.location.href = "public/home.html";
         }else if (this.readyState == 4 && this.status >= 400){
             alert("Login Failed");
         }
