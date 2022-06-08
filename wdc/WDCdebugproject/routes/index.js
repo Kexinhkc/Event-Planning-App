2fd3cf599a4f0f351fe4190bc457682b14b2825e
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-08 08:44:55 +0000

    Wed, Jun 8, 2022, 8:44 AM +00:00

--- wdc/WDCdebugproject/routes/index.js
+++ wdc/WDCdebugproject/routes/index.js
@@ -228,62 +228,62 @@ router.post('/userAccount', function(req, res, next) {
           });
         });
 
-  }else if ('token' in req.body) {
+   } //else if ('token' in req.body) {
 
-        let email =null;
-        async function verify() {
-          const ticket = await client.verifyIdToken({
-              idToken: req.body.token,
-              audience: '1006345318538-06u5fqd3sur2bthaea307jtjalc33f3r.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
-              // Or, if multiple clients access the backend:
-              //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-          });
-          const payload = ticket.getPayload();
-          const userid = payload['sub'];
-          console.log(userid)
-          email = payload['email'];
-          // email = payload['email'];
-        }
-        // verify().then(function).catch(console.error);//////
-        verify().then(function(){
+  //       let email =null;
+  //       async function verify() {
+  //         const ticket = await client.verifyIdToken({
+  //             idToken: req.body.token,
+  //             audience: '1006345318538-06u5fqd3sur2bthaea307jtjalc33f3r.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
+  //             // Or, if multiple clients access the backend:
+  //             //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  //         });
+  //         const payload = ticket.getPayload();
+  //         const userid = payload['sub'];
+  //         console.log(userid)
+  //         email = payload['email'];
+  //         // email = payload['email'];
+  //       }
+  //       // verify().then(function).catch(console.error);//////
+  //       verify().then(function(){
 
-          req.pool.getConnection(function(error,connection){
-            if(error){
-              console.log("connection error")
-              console.log(error);
-              res.sendStatus(500);
-              return;
-            }
+  //         req.pool.getConnection(function(error,connection){
+  //           if(error){
+  //             console.log("connection error")
+  //             console.log(error);
+  //             res.sendStatus(500);
+  //             return;
+  //           }
 
-            let query = "SELECT email FROM admin WHERE email = ?;";
-            connection.query(query,[email],function(error, rows, fields) {
-              connection.release(); // release connection
-              if (error) {
-                console.log("query error")
-                console.log(error);
-                res.sendStatus(500);
-                return;
-              }
+  //           let query = "SELECT email FROM admin WHERE email = ?;";
+  //           connection.query(query,[email],function(error, rows, fields) {
+  //             connection.release(); // release connection
+  //             if (error) {
+  //               console.log("query error")
+  //               console.log(error);
+  //               res.sendStatus(500);
+  //               return;
+  //             }
 
-              if (rows.length > 0){
-                console.log('success');
-                //req.session.user = row[0];
-                res.sendStatus(200);
-              }else{
-                console.log('bad login');
-                res.sendStatus(401);
-              }
+  //             if (rows.length > 0){
+  //               console.log('success');
+  //               //req.session.user = row[0];
+  //               res.sendStatus(200);
+  //             }else{
+  //               console.log('bad login');
+  //               res.sendStatus(401);
+  //             }
 
-              });
-            });
-          }).catch(function(){
-          res.sendStatus(401);
-        });
+  //             });
+  //           });
+  //         }).catch(function(){
+  //         res.sendStatus(401);
+  //       });
 
-  } else {
-    console.log('bad request');
-    res.sendStatus(400);
-  }
+  // } else {
+  //   console.log('bad request');
+  //   res.sendStatus(400);
+  // }
 
 });
 
