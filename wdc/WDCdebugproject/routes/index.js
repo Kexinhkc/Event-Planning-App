cb46a0204395eb9c3008df59ae592bff3ff65e19
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-08 08:39:55 +0000

    Wed, Jun 8, 2022, 8:39 AM +00:00

--- wdc/WDCdebugproject/routes/index.js
+++ wdc/WDCdebugproject/routes/index.js
@@ -8,6 +8,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'landingPage' });
 });
 
+////////////////////////////////////////////////////////////////////////////////
+
 router.post('/login', function(req, res, next) {
 
   console.log("inside route")
@@ -103,10 +105,9 @@ router.post('/login', function(req, res, next) {
 
 });
 
+////////////////////////////////////////////////////////////////////////////////
 
 router.post('/adminLogin', function(req, res, next) {
-
-
 
   if ('email' in req.body && 'password' in req.body) {
 
@@ -199,7 +200,102 @@ router.post('/adminLogin', function(req, res, next) {
 
 });
 
+////////////////////////////////////////////////////////////////////////////////
 
-  
+router.post('/userAccount', function(req, res, next) {
+
+  if ('email' in req.body && 'password' in req.body) {
+
+      req.pool.getConnection(function(error,connection){
+        if(error){
+          console.log("connection error")
+          console.log(error);
+          res.sendStatus(500);
+          return;
+        }
+
+        let query = "INSERT INTO users VALUES ()FROM admin WHERE email = ? AND password = ?;";
+        connection.query(query,[req.body.email,req.body.password],function(error, rows, fields) {
+          connection.release(); // release connection
+          if (error) {
+            console.log("query error")
+            console.log(error);
+            res.sendStatus(500);
+            return;
+          }
+
+          if (rows.length > 0){
+            console.log('success');
+            //req.session.user = row[0];
+            res.sendStatus(200);
+          }else{
+            console.log('bad login');
+            res.sendStatus(401);
+          }
+
+          });
+        });
+
+  }else if ('token' in req.body) {
+
+        let email =null;
+        async function verify() {
+          const ticket = await client.verifyIdToken({
+              idToken: req.body.token,
+              audience: '1006345318538-06u5fqd3sur2bthaea307jtjalc33f3r.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
+              // Or, if multiple clients access the backend:
+              //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+          });
+          const payload = ticket.getPayload();
+          const userid = payload['sub'];
+          console.log(userid)
+          email = payload['email'];
+          // email = payload['email'];
+        }
+        // verify().then(function).catch(console.error);//////
+        verify().then(function(){
+
+          req.pool.getConnection(function(error,connection){
+            if(error){
+              console.log("connection error")
+              console.log(error);
+              res.sendStatus(500);
+              return;
+            }
+
+            let query = "SELECT email FROM admin WHERE email = ?;";
+            connection.query(query,[email],function(error, rows, fields) {
+              connection.release(); // release connection
+              if (error) {
+                console.log("query error")
+                console.log(error);
+                res.sendStatus(500);
+                return;
+              }
+
+              if (rows.length > 0){
+                console.log('success');
+                //req.session.user = row[0];
+                res.sendStatus(200);
+              }else{
+                console.log('bad login');
+                res.sendStatus(401);
+              }
+
+              });
+            });
+          }).catch(function(){
+          res.sendStatus(401);
+        });
+
+  } else {
+    console.log('bad request');
+    res.sendStatus(400);
+  }
+
+});
+
+
+
 
 module.exports = router;
