191df9388e99ccbc816c79e35b962a74d615b8c8
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-08 09:08:37 +0000

    Wed, Jun 8, 2022, 9:08 AM +00:00

--- wdc/WDCdebugproject/routes/index.js
+++ wdc/WDCdebugproject/routes/index.js
@@ -294,4 +294,143 @@ router.post('/userAccount', function(req, res, next) {
 
 
 
+router.post('/searchUsers', function (req, res, next) {
+  //Connect to the database
+  req.pool.getConnection( function(err,connection) {
+    if (err) {
+      res.sendStatus(500);
+      console.log(err);
+      return;
+    }
+
+    var query = "SELECT * FROM users WHERE email = ?;";
+    console.log(req.body);
+    connection.query(query, [req.body.email], function (err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        console.log(err);
+        return;
+      }
+      console.log(rows);
+      res.json(rows[0]);
+    });
+  });
+});
+
+router.post('/searchEvents', function (req, res, next) {
+  //Connect to the database
+  req.pool.getConnection( function(err,connection) {
+    if (err) {
+      res.sendStatus(500);
+      console.log(err);
+      return;
+    }
+
+    var query = "SELECT * FROM events WHERE event_name = ?;";
+    connection.query(query, [req.body.event_name], function (err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        console.log(err);
+        return;
+      }
+      res.json(rows[0]);
+    });
+  });
+});
+
+router.post('/manageUsers', function (req, res, next) {
+  //Connect to the database
+  req.pool.getConnection( function(err,connection) {
+    if (err) {
+      res.sendStatus(500);
+      console.log(err);
+      return;
+    }
+
+    var query = "UPDATE users SET password = ?, first_name = ?, last_name = ? WHERE email = ?;";
+    connection.query(query, [req.body.password,req.body.first_name,req.body.last_name,req.body.email], function (err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        console.log(err);
+        return;
+      }
+      res.sendStatus(200);
+    });
+  });
+});
+
+router.post('/manageEvents', function (req, res, next) {
+  //Connect to the database
+  req.pool.getConnection( function(err,connection) {
+    if (err) {
+      res.sendStatus(500);
+      console.log(err);
+      return;
+    }
+
+    var query = "UPDATE events SET description = ? WHERE event_name = ?;";
+    connection.query(query, [req.body.d,req.body.event], function (err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        console.log(err);
+        return;
+      }
+      res.sendStatus(200);
+    });
+  });
+});
+
+router.post('/deleteUsers', function (req, res, next) {
+  //Connect to the database
+  req.pool.getConnection( function(err,connection) {
+    if (err) {
+      res.sendStatus(500);
+      console.log(err);
+      return;
+    }
+
+    var query = "DELETE FROM users WHERE email = ?;";
+    console.log(req.body);
+    connection.query(query, [req.body.email], function (err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        console.log(err);
+        return;
+      }
+      res.sendStatus(200);
+    });
+  });
+});
+
+router.post('/deleteEvents', function (req, res, next) {
+  //Connect to the database
+  req.pool.getConnection( function(err,connection) {
+    if (err) {
+      res.sendStatus(500);
+      console.log(err);
+      return;
+    }
+
+    var query = "DELETE FROM events WHERE event_name = ?;";
+    console.log(req.body);
+    connection.query(query, [req.body.event], function (err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        console.log(err);
+        return;
+      }
+      res.sendStatus(200);
+    });
+  });
+});
+
+
+
+
 module.exports = router;
