9b76ade404db471c5c2e02a2a3b58d24f44a3536
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-08 08:11:47 +0000

    Wed, Jun 8, 2022, 8:11 AM +00:00

--- wdc/WDCdebugproject/public/javascripts/client.js
+++ wdc/WDCdebugproject/public/javascripts/client.js
@@ -52,25 +52,21 @@ function onSignIn(googleUser) {
 
   }
 
-  function onSignIn(googleUser) {
+  function onSignIn2(googleUser) {
     var profile = googleUser.getBasicProfile();
-    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    // console.log('Name: ' + profile.getName());
-    // console.log('Image URL: ' + profile.getImageUrl());
-    // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
     let xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             alert("Login Successfully");
-           window.location.replace("home.html");
+           window.location.replace("homeAdmin.html");
         }else if (this.readyState == 4 && this.status >= 400){
             alert("Login Failed");
         }
     };
 
-    xhttp.open("POST", "/login");
+    xhttp.open("POST", "/adminLogin");
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send(JSON.stringify({token : googleUser.getAuthResponse().id_token}));
 
