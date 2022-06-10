f20f8a1da8e941e44ef195ef398d8378026578e7
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-10 12:27:02 +0000

    Fri, Jun 10, 2022, 12:27 PM +00:00

--- wdc/WDCdebugproject/routes/index.js
+++ wdc/WDCdebugproject/routes/index.js
@@ -597,15 +597,15 @@ router.get('/profile', (req, res, next) => {
   }
   else
   {
-    email = req.session.user;
-    console.log(email);
+  email = req.session.user;
+  console.log(email);
   req.pool.getConnection( function(err,connection) {
          if (err) {
            res.sendStatus(500);
            return;
          }
-         var query = "SELECT * FROM users WHERE email = ?;";
-         connection.query(query, [req.session.user], function(err, rows, fields) {
+         var query = "SELECT * FROM users WHERE email = '"+email+"';";
+         connection.query(query, function(err, rows, fields) {
          connection.release(); // release connection
 
        //   if (err)
