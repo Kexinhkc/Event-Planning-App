5ecbc7056b5b36c3ae2d4c2ea24ccc97c165e360
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-10 04:43:36 +0000

    Fri, Jun 10, 2022, 4:43 AM +00:00

--- wdc/WDCdebugproject/public/javascripts/client.js
+++ wdc/WDCdebugproject/public/javascripts/client.js
@@ -62,7 +62,8 @@ function onSignIn(googleUser) {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
-      
+      delete req.session.user;
+
     });
   }
 
