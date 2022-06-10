d11621de8512a31130ae6acd25f30e6e659878d7
Kexinhkc <64817956+Kexinhkc@users.noreply.github.com>
2022-06-10 10:51:29 +0000

    Fri, Jun 10, 2022, 10:51 AM +00:00

--- wdc/WDCdebugproject/public/javascripts/client.js
+++ wdc/WDCdebugproject/public/javascripts/client.js
@@ -197,29 +197,29 @@ function login() {
 
 
 
-  function showUser()
-{
-    let users = {
-        email: document.getElementsByName("userSearch")[0].value
-      };
+//   function showUser()
+// {
+//     let users = {
+//         email: document.getElementsByName("userSearch")[0].value
+//       };
 
-      console.log(users);
+//       console.log(users);
 
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var obj = JSON.parse(this.response);
-            console.log(obj);
-            alert("search Successful");
-            window.location.replace(`./manageUser.html?email=${obj.email}&last_name=${obj.last_name}&first_name=${obj.first_name}&password=${obj.password}`);
-        }
-      };
-      xhttp.open("POST", "/searchUsers");
-      // Set content type to JSON
-      xhttp.setRequestHeader("Content-type", "application/json");
-      // Send request
-      xhttp.send(JSON.stringify(users));
-}
+//         var xhttp = new XMLHttpRequest();
+//         xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             var obj = JSON.parse(this.response);
+//             console.log(obj);
+//             alert("search Successful");
+//             window.location.replace(`./manageUser.html?email=${obj.email}&last_name=${obj.last_name}&first_name=${obj.first_name}&password=${obj.password}`);
+//         }
+//       };
+//       xhttp.open("POST", "/searchUsers");
+//       // Set content type to JSON
+//       xhttp.setRequestHeader("Content-type", "application/json");
+//       // Send request
+//       xhttp.send(JSON.stringify(users));
+// }
 
 
 function showEvent()
@@ -342,4 +342,116 @@ function deleteEvent()
   xhttp.setRequestHeader("Content-type", "application/json");
   // Send request
   xhttp.send(JSON.stringify(events));
+}
+
+function createAdmin()
+{
+  let admin = {
+    email: document.getElementsByName("input-admin-email")[0].value,
+    password: document.getElementsByName("input-admin-password")[0].value
+  };
+  console.log(admin);
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        alert("Add Successful");
+    }
+  };
+  xhttp.open("POST", "/createAdmins");
+  // Set content type to JSON
+  xhttp.setRequestHeader("Content-type", "application/json");
+  // Send request
+  xhttp.send(JSON.stringify(admin));
+}
+
+function pickTime()
+{
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.response);
+      var obj = JSON.parse(this.response);
+      console.log(obj);
+      console.log(obj.timestamp);
+      document.getElementsByName("finalDate")[0].innerHTML= obj.timestamp;
+        //alert("Add Successful");
+    }
+  };
+  xhttp.open("GET", "/showTime");
+  xhttp.send();
+}
+
+function collectTime()
+{
+  let available = {
+    date: document.getElementsByName("time")[0].value,
+    id: document.getElementsByName("ID")[0].value,
+  };
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        alert("Add Successful");
+    }
+  };
+  xhttp.open("POST", "/collectTimes");
+  // Set content type to JSON
+  xhttp.setRequestHeader("Content-type", "application/json");
+  // Send request
+  xhttp.send(JSON.stringify(available));
+}
+
+function createNewEvent()
+{
+  let event = {
+    name: document.getElementsByName("eventName")[0].value,
+    d: document.getElementsByName("eventD")[0].value,
+  };
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        alert("Add Successful");
+    }
+  };
+  xhttp.open("POST", "/createEvents");
+  // Set content type to JSON
+  xhttp.setRequestHeader("Content-type", "application/json");
+  // Send request
+  xhttp.send(JSON.stringify(event));
+}
+
+
+function userEvent()
+{
+  let events = {
+    event_name: document.getElementsByName("eventDetails2")[0].innerText
+  };
+
+  console.log(events);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.response);
+        var obj = JSON.parse(this.response);
+        console.log(obj);
+        alert("Show Successful");
+        window.location.replace(`./eventDetail.html?event_name=${obj.event_name}&description=${obj.description}&ID=${obj.ID}`);
+
+    }
+  };
+  xhttp.open("POST", "/userEvents");
+  // Set content type to JSON
+  xhttp.setRequestHeader("Content-type", "application/json");
+  // Send request
+  xhttp.send(JSON.stringify(events));
+}
+
+function loadUserEvent()
+{
+  const urlParams = new URLSearchParams(window.location.search);
+  document.getElementsByName("user-event-name")[0].value = urlParams.get('event_name');
+  document.getElementsByName("user-event-ID")[0].value = urlParams.get('ID');
+  document.getElementsByName("user-event-d")[0].value = urlParams.get('description');
 }
